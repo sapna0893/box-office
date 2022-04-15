b0a2be961e4e1bb15c9fe27738c277da7fe5dd91
@DreamWebApp <sapnachoithani0893@gmail.com>
2022-04-15 23:47:16 +0530

    added react-router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div >
-      Hi
-    </div>
+    <Switch>
+      <Route exact path="/">
+        This is the HomePage.
+      </Route>
+      <Route exact path="/starred">
+        This is the Starred Page.
+      </Route>
+      <Route>
+        This is 404 Page.
+      </Route>
+    </Switch>
   );
 }
 
