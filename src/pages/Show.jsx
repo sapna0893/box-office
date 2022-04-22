144a38cbf98a340980af2b90319f0d5f9685da74
@DreamWebApp <sapnachoithani0893@gmail.com>
2022-04-22 18:14:50 +0530

    added error and loading messages to the show page

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -5,13 +5,37 @@ import { apiGet } from '../misc/config';
 function Show() {
   const { id } = useParams();
   const [show, setShow] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [errors, setErrors] = useState(null);
   useEffect(() => {
-    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(results => {
-      setShow(results);
-    });
+    let isMounted = true;
+    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
+      .then(results => {
+        if (isMounted) {
+          setShow(results);
+          setIsLoading(false);
+        }
+      })
+      .catch(err => {
+        if (isMounted) {
+          setErrors(err.message);
+          setIsLoading(false);
+        }
+      });
+
+    return () => {
+      isMounted = false;
+    };
   }, [id]);
 
   console.log('show', show);
+
+  if (isLoading) {
+    return <div>Data is getting loaded</div>;
+  }
+  if (errors) {
+    return <div>Error occured: {errors}</div>;
+  }
   return <div>This is Show Page</div>;
 }
 
